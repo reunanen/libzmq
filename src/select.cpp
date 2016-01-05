7f0f9974031a5b65a1538251a511503eb3a2175a
Juha Reunanen <juha.reunanen@tomaattinen.com>
2016-01-05 16:28:03 +0200

    Try to recover on WSAENOTSOCK.

--- src/select.cpp
+++ src/select.cpp
@@ -182,7 +182,21 @@ void zmq::select_t::loop ()
 #ifdef ZMQ_HAVE_WINDOWS
         int rc = select (0, &readfds, &writefds, &exceptfds,
             timeout ? &tv : NULL);
-        wsa_assert (rc != SOCKET_ERROR);
+
+        if (rc == SOCKET_ERROR) {
+            DWORD lastError = GetLastError();
+            if (lastError == WSAENOTSOCK) {
+                // Sometimes, on Windows, we got this. So... let's try to do something.
+                for (fd_set_t::size_type i = 0; i < fds.size(); i++) {
+                    if (fds[i].fd != retired_fd) {
+                        fds[i].events->in_event();
+                    }
+                }
+            }
+            else {
+                wsa_assert (rc != SOCKET_ERROR);
+            }
+        }
 #else
         int rc = select (maxfd + 1, &readfds, &writefds, &exceptfds,
             timeout ? &tv : NULL);
